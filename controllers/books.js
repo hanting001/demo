84bbda9a40813a2cdd149cf44ff927fa07c3f7a0
hanting001 <hanting001@gmail.com>
2014-05-27 17:24:22 +0800

    增加rest服务调用代码

--- controllers/books.js
+++ controllers/books.js
@@ -1,30 +1,30 @@
 var http = require('http');
-
+var rest = require('../lib/rest');
+var nconf = require('nconf');
+var pagination = nconf.get('pagination');
 module.exports = function(app) {
 	app.get('/books', function(req, res, next) {
-		var options = {
-			host : 'localhost',
-			port : 8080,
-			path : '/apis/books?format=json',
-			method : 'GET',
-			headers : {
-				'accept' : '*/*',
-				'content-type' : "application/json"
-			}
-		};
-		var books = '';
+		var page = 1;
+		if (req.query.page) {
+			page = req.query.page;
+		}
+		console.log(page);
+		var options = rest.getOption('/apis/book?format=json' + '&offset=' + (page - 1) * 2 + '&max=2', 'GET');
+		var result = '';
 		var req = http.request(options, function(response) {
 			//console.log('STATUS: ' + res.statusCode);
 			//console.log('HEADERS: ' + JSON.stringify(res.headers));
 			response.setEncoding('utf8');
 			response.on('data', function(chunk) {
-				books = books + chunk;
+				result = result + chunk;
 			});
 			response.on('end', function () {
-	            //console.log(books);
+				var resultObject = JSON.parse(result);
 	            var model = {
-	            	title : '书目',
-	            	books : JSON.parse(books)
+	            	title 		: '书目',
+	            	books 		: resultObject.books,
+	            	page  		: page,
+	            	pageCount	: resultObject.total/2 + resultObject.total % 2
 	            };
 	            res.render('books/index', model);  
 	        });
@@ -32,5 +32,53 @@ module.exports = function(app) {
 
 		// write data to request body
 		req.end();
+	});
+	app.post('/books/create', function(req, res, nex){
+		var book = req.body.book;
+		var result = '';
+		var options = rest.getOption('/apis/book?format=json', 'POST');
+		var req = http.request(options, function(response) {
+			//console.log('HEADERS: ' + JSON.stringify(response.headers));
+			response.on('data', function(chunk) {
+				result = result + chunk;
+			});
+			response.on('end', function () {
+	            res.redirect('/books');  
+	        });
+		});
+		// write data to request body
+		req.write(JSON.stringify(book));
+		req.end();
+	});
+	
+	app.post('/books/query', function(req, res, nex){
+		var page = 1;
+
+		var book = req.body.book;
+		if (book.title) {
+			var result = '';
+			var options = rest.getOption('/apis/book/findByTitle?format=json', 'POST');
+			var req = http.request(options, function(response) {
+				//console.log('HEADERS: ' + JSON.stringify(response.headers));
+				response.on('data', function(chunk) {
+					result = result + chunk;
+				});
+				response.on('end', function () {
+					var books = JSON.parse(result);
+					var model = {
+			            	title : '书目',
+			            	book  : book,
+			            	books : books
+			        };
+			        res.render('books/index', model);  
+		        });
+			}); 
+			// write data to request body
+			req.write(JSON.stringify(book));
+			req.end();			
+		} else {
+			res.redirect('/books');
+		}
+
 	});
 };
