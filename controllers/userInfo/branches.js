16466e61ae7bccf9db1b5126dce941a80b135e4f
hanting001 <hanting001@gmail.com>
2014-06-19 17:44:53 +0800

    0619

--- controllers/userInfo/branches.js
+++ controllers/userInfo/branches.js
@@ -18,7 +18,8 @@ module.exports = function(app) {
 					isAdmin : true,
 					branches : branches,
 					page : page,
-					pageCount : pageCount
+					pageCount : pageCount,
+					showMessage : req.flash('showMessage')
 				};			
 			res.render('userInfo/branches/index', model);
 		}, { sortBy : { code : 1 } });
@@ -47,7 +48,7 @@ module.exports = function(app) {
 		branch.status = '1';
 		//添加其它信息省略
 		//...
-		
+		console.log(22222);
 		var branchModel = new Branch(branch);
 		branchModel.save(function(err){
 			if(err) {
@@ -56,10 +57,11 @@ module.exports = function(app) {
 						parent : {id : id, abbrName : req.body.parentAbbr}
 				};
 				res.locals.err = err;
-				res.locals.url = 'userInfo/branches/addSub';
+				res.locals.view = 'userInfo/branches/addSub';
 				res.locals.model = model;
 				next();//调用下一个错误处理middlewear
 			} else {
+				req.flash('showMessage', '创建成功');
 				res.redirect('/branches');
 			}
 		});
@@ -71,13 +73,46 @@ module.exports = function(app) {
 			if (err) {
 				return next(err);
 			}
-			var model = {
+			Branch.findById(branch.parent, 'abbrName', function(err, parent){
+				console.log(parent);
+				var model = {
 					title : '编辑机构信息',
 					isAdmin : true,
-					branch : branch
+					branch : branch,
+					parent : parent,
+					showMessage : req.flash('showMessage')
 				};			
-			res.render('userInfo/branches/edit', model);
+				res.render('userInfo/branches/edit', model);
+			});
 		});
 	});
-	
+	app.post('/branches/:id/edit', function(req, res, next) {
+		var id = req.params.id;
+		//var branchNew = req.body.branch;
+		//console.log(branch);
+		Branch.findOne({_id : new ObjectId(id)}, function(err, branch){
+			if (!err) {
+				var newBranch = req.body.branch;
+				for (var o in newBranch) {
+					branch[o] = newBranch[o];
+				}
+				branch.save(function(err, branch){
+					if(err) {
+						newBranch.id = id;
+						var model = {
+								branch : newBranch,
+								parent : {id : req.body.parentId, abbrName : req.body.parentAbbr}
+						};
+						res.locals.err = err;
+						res.locals.view = 'userInfo/branches/edit';
+						res.locals.model = model;
+						next();//调用下一个错误处理middlewear
+					} else {
+						req.flash('showMessage', '修改成功');
+						res.redirect('/branches/' + branch.id + '/edit');
+					}					
+				});
+			}			
+		});
+	});
 };
