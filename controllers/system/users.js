7b9b68a76bc80a58b1c0f996a5260fe6eb6d8970
hanting001 <hanting001@gmail.com>
2014-07-05 15:52:07 +0800

    恢复users.js

--- controllers/system/users.js
+++ controllers/system/users.js
@@ -1,31 +1,16 @@
 var passport = require('passport');
 var auth = require('../../lib/auth');
 var User = require('../../models/system/User');
-<<<<<<< HEAD
-module.exports = function (app) {
-	app.get('/system/users/signup', function(req, res) {
-=======
 var UserInfo = require('../../models/UserInfo');
 var Role = require('../../models/system/Role');
 module.exports = function(app) {
 	app.get('/system/auth/users/signup', function(req, res) {
->>>>>>> 364f83975fe2c48da3faa207d1041957edb32ed6
 		// render the page and pass in any flash data if it exists
 		res.render('system/users/signupForm', {
 			title: '注册用户',
 			message: req.flash('error')
 		});
 	});
-<<<<<<< HEAD
-	
-	app.post('/system/users/signup', passport.authenticate('local-signup', {
-		successRedirect : '/', // redirect to the secure profile section
-		failureRedirect : '/users', // redirect back to the signup page if there is an error
-		failureFlash : true // allow flash messages
-	}));
-	
-	app.get('/system/users', auth.isAuthenticated('ROLE_ADMIN'), function(req, res, next){
-=======
 
 	app.post('/system/auth/users/signup', passport.authenticate('local-signup', {
 		successRedirect: '/', // redirect to the secure profile section
@@ -34,7 +19,6 @@ module.exports = function(app) {
 	}));
 
 	app.get('/system/auth/users', auth.isAuthenticated('ROLE_ADMIN'), function(req, res, next) {
->>>>>>> 364f83975fe2c48da3faa207d1041957edb32ed6
 		var page = 1;
 		if (req.query.page) {
 			page = req.query.page;
