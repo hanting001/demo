1ca564505e706136e4963f5d53bae00b52b1a751
hanting001 <hanting001@gmail.com>
2014-07-08 17:14:07 +0800

    完善用户管理模块

--- models/system/User.js
+++ models/system/User.js
@@ -1,39 +1,66 @@
-var mongoose = require('mongoose'), 
-	bcrypt = require('bcrypt'), 
-	nconf = require('nconf');
+var mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    nconf = require('nconf');
 var updatedTimestamp = require('mongoose-updated_at');
 var uniqueValidator = require('mongoose-unique-validator');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 var userSchema = new mongoose.Schema({
-	name		: {type : String, unique : true, required : true},
-	password	: {type : String, unique : true},
-    fullName    : String,
-	roles		: {type:[String], default:['ROLE_USER']},
-    userInfo    : {type:ObjectId, ref:'UserInfo'},
-	branchCode	: {type:String},
-	userType	: {type:String, default:'1'},
-	oprBranches	: [String],
-	isValid		: {type:String, default:'1'},
-	createdAt	: {type: Date, default: Date.now }
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        unique: true
+    },
+    fullName: String,
+    roles: {
+        type: [String],
+        default: ['ROLE_USER']
+    },
+    userInfo: {
+        type: ObjectId,
+        ref: 'UserInfo'
+    },
+    branch: {
+        type: String
+    },
+    userType: {
+        type: String,
+        default: '1'
+    },
+    oprBranches: [String],
+    isValid: {
+        type: String,
+        default: '1'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 //添加create、update字段
 userSchema.plugin(updatedTimestamp);
 //添加唯一字段校验
-userSchema.plugin(uniqueValidator, { message: '出错拉, {PATH}不能同已有值重复' });
+userSchema.plugin(uniqueValidator, {
+    message: '出错拉, {PATH}不能同已有值重复'
+});
 /**
  * Helper function that hooks into the 'save' method, and replaces plaintext passwords with a hashed version.
  */
-userSchema.pre('save', function (next) {
+userSchema.pre('save', function(next) {
     var user = this;
+    if (this.oprBranches && this.oprBranches.length == 0) {
+        this.oprBranches = [this.branch];
+    }
 
+    
     //If the password has not been modified in this save operation, leave it alone (So we don't double hash it)
     if (!user.isModified('password')) {
         next();
         return;
-    }
-    if (!this.oprBranches) {
-    	this.oprBranches = [this.branchCode];
     }
     //Retrieve the desired difficulty from the configuration. (Default = 8)
     var DIFFICULTY = (nconf.get('bcrypt') && nconf.get('bcrypt').difficulty) || 8;
@@ -51,5 +78,7 @@ userSchema.pre('save', function (next) {
 userSchema.methods.validPassword = function(plainText) {
     return bcrypt.compareSync(plainText, this.password);
 };
-
+userSchema.methods.textPassword = function(password) {
+    return bcrypt.compareSync(plainText, this.password);
+};
 module.exports = mongoose.model('User', userSchema);
